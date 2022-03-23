65dc85ca11c612f5d3ace9ea6d4257cf6c8fc537
Gino Tasis <gtasis84@gmail.com>
2022-03-23 16:25:59 -0400

    Create model file and add all images to assets from images folder

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,20 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Gino Tasis on 3/23/22.
+//
+
+import Foundation
+
+struct Landmark: Codable {
+    
+    let name: String
+    let category: String
+    let city: String
+    let state: String
+    let id: Int
+    let park: String
+    let description: String
+    
+}
