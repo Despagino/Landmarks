2a32327e8ed6ed668e527bc7fa1d4a6efbaebdd3
Gino Tasis <gtasis84@gmail.com>
2022-03-24 01:02:30 -0400

    Add Observable property wrapper into a new object and added the landmarks array into it with @Published

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -41,6 +41,7 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+.previewInterfaceOrientation(.landscapeRight)
         }
     }
 
