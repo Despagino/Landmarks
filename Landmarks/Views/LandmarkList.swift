45e9898264c8ff3c6aaad0913bf1397de40f1e08
Gino Tasis <gtasis84@gmail.com>
2022-03-24 00:34:22 -0400

    Add state property in landmark list to dynamically add a start whenever isfavorite is true

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = false
+    
     var body: some View {
         NavigationView {
             List(landmarks) { landmark in
@@ -23,11 +26,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-
         }
     }
-}
+
