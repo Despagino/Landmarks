d850ffc60c0bd0b5fc54811938561598bda7603a
Gino Tasis <gtasis84@gmail.com>
2022-03-23 23:56:57 -0400

    Fix some layout bugs with circle

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -10,21 +10,19 @@ import SwiftUI
 struct CircleImage: View {
     
     var image: Image
-    
+
     var body: some View {
         image
-            .resizable()
-            .scaledToFit()
             .clipShape(Circle())
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
-        }
+            }
             .shadow(radius: 7)
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage(image: landmarks[0].image)
+        CircleImage(image: Image("retro"))
     }
 }
